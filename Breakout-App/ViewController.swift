d7e53d4a7d3b3d166202539da4029203ea2f61a6
student8 <student8@jh-mba158-22.d214.org>
2017-04-28 14:39:27 -0500

    BALL SHAPE

--- Breakout-App/ViewController.swift
+++ Breakout-App/ViewController.swift
@@ -9,16 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var ballView: UIView!
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        ballView.layer.cornerRadius = 15
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+
 
 
 }

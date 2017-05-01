4de4c71a7c012413b604b86abd481519b22a001d
Jojo5291 <jovanymelchor@yahoo.com>
2017-05-01 14:09:42 -0500

    ball is moving =D

--- Breakout-App/ViewController.swift
+++ Breakout-App/ViewController.swift
@@ -8,23 +8,80 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollisionBehaviorDelegate {
     
     @IBOutlet weak var ballView: UIView!
     
+    @IBOutlet weak var paddleView: UIView!
     
+    var dynamicAnimator: UIDynamicAnimator!
     
+    var pushBehavior: UIPushBehavior!
+    
+    var collisionBehavior: UICollisionBehavior!
+    
+    var ballDynamicBehavior: UIDynamicItemBehavior!
+    
+    var paddleDynamicBehavior: UIDynamicItemBehavior!
     
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         ballView.layer.cornerRadius = 15
-
+        
     }
-
-
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        ballView.layer.cornerRadius = 12.0
+        
+        dynamicAnimator = UIDynamicAnimator(referenceView: self.view)
+        
+        pushBehavior = UIPushBehavior(items: [ballView], mode: UIPushBehaviorMode.instantaneous)
+        
+        pushBehavior.pushDirection = CGVector(dx: 0.5, dy: 1.0)
+        
+        pushBehavior.active = true
+        
+        pushBehavior.magnitude = 0.5
+        
+        dynamicAnimator.addBehavior(pushBehavior)
+        
+        collisionBehavior = UICollisionBehavior(items: [ballView, paddleView])
+        
+        collisionBehavior.collisionMode = UICollisionBehaviorMode.everything
+        
+        collisionBehavior.translatesReferenceBoundsIntoBoundary = true
+        
+        collisionBehavior.collisionDelegate = self
+        
+        dynamicAnimator.addBehavior(collisionBehavior)
+        
+        ballDynamicBehavior = UIDynamicItemBehavior(items: [ballView])
+        
+        ballDynamicBehavior.allowsRotation = false
+        
+        ballDynamicBehavior.elasticity = 1.0
+        
+        ballDynamicBehavior.resistance = 0.0
+        
+        ballDynamicBehavior.friction = 0.0
+        
+        dynamicAnimator.addBehavior(ballDynamicBehavior)
+        
+        paddleDynamicBehavior = UIDynamicItemBehavior(items: [paddleView])
+        
+        paddleDynamicBehavior.allowsRotation = false
+        
+        paddleDynamicBehavior.density = 1000.0
+        
+        dynamicAnimator.addBehavior(paddleDynamicBehavior)
+        
+        
+        
+    }
+    
+    
 }
 
